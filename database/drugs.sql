9a16a222aac107e0eae843b3330a6fc8f02a838e
duonghugama <83336105+duonghugama@users.noreply.github.com>
2021-10-21 13:29:40 +0700

    Bài 1

---
+++ database/drugs.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2021 at 08:28 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `drugs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drugs`
+--
+
+CREATE TABLE `drugs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `barcode` int(11) NOT NULL,
+  `dose` int(11) NOT NULL,
+  `cost_price` float NOT NULL,
+  `selling_pricve` float NOT NULL,
+  `expiry` tinyint(1) NOT NULL,
+  `company_name` varchar(100) NOT NULL,
+  `production_date` date NOT NULL,
+  `expiration_date` date NOT NULL,
+  `place` varchar(100) DEFAULT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `drugs`
+--
+
+INSERT INTO `drugs` (`id`, `name`, `type`, `barcode`, `dose`, `cost_price`, `selling_pricve`, `expiry`, `company_name`, `production_date`, `expiration_date`, `place`, `quantity`) VALUES
+(1, 'Astra', 'vaccine', 1, 1, 1000, 1200, 0, 'AstraZeneca', '2021-03-03', '2021-10-13', 'UK', 10),
+(2, 'Moderna', 'vaccine', 2, 1, 900, 1000, 0, 'AstraZeneca', '2021-01-01', '2021-10-01', 'China', 9),
+(3, 'Panadol', 'viên nén', 11, 2, 100, 200, 1, 'Panadol', '2018-04-04', '2021-10-01', 'US', 100);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `drugs`
+--
+ALTER TABLE `drugs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `drugs`
+--
+ALTER TABLE `drugs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
